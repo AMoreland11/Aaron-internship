df72a224f327be7e41d4a626af2f7d7f0f7ded32
Aaron Moreland <a11daaron@gmail.com>
2025-05-26 16:41:52 -0400

    adding dynamic cards for explore page

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -70,7 +70,7 @@ const ExploreItems = () => {
             <div className="nft__item">
               <div className="author_list_pp">
                 <Link
-                  t={`/author/${item.authorId}`}
+                  to={`/author/${item.authorId}`}
                   data-bs-toggle="tooltip"
                   data-bs-placement="top"
                   title={`Creator: ${item.authorName}`}
@@ -80,9 +80,37 @@ const ExploreItems = () => {
                     src={item.authorImage}
                     alt={item.authorName}
                   />
+                  <i className="fa fa-check"></i>
                 </Link>
               </div>
               <CountdownTimer expiryDate={item.expiryDate} />
+
+              <div className="nft__item_wrap">
+                <div className="nft__item_extra">
+                  <div className="nft__item_buttons">
+                    <button>Buy Now</button>
+                    <div className="nft__item_share">
+                      <h4>Share</h4>
+                      <a href="" target="_blank" rel="noreferrer">
+                        <i className="fa fa-facebook fa-lg"></i>
+                      </a>
+                      <a href="" target="_blank" rel="noreferrer">
+                        <i className="fa fa-twitter fa-lg"></i>
+                      </a>
+                      <a href="" target="_blank" rel="noreferrer">
+                        <i className="fa fa-envelope fa-lg"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <Link to={`/item-details/${item.nftId}`}>
+                  <img
+                    src={item.nftImage}
+                    className="lazy nft__item_preview"
+                    alt={item.title}
+                  />
+                </Link>
+              </div>
             </div>
           </div>
         ))
